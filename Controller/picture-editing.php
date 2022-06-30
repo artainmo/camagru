8bc66499f6e5c298b9c79d6953877e0289fbfb24
artainmo <tainmontarthur@icloud.com>
2022-06-30 20:59:23 +0200

    When camara is not activated local images can be used

--- Controller/picture-editing.php
+++ Controller/picture-editing.php
@@ -17,8 +17,7 @@
 <?php require(__DIR__ . "/../View/header/in-app-header.php"); ?>
 
 <h3>Take Picture</h3>
-<p id="error"></p>
-<video width='320' height='240' autoplay></video><br>
+<div id="getPicture"></div>
 <canvas id="takePictureCanvas" style="display:none;" width="320" height="240"></canvas>
 <form id="takePictureButton">
 	<p>Choose Overlay Image:</p>
@@ -46,6 +45,20 @@
 <?php require(__DIR__ . "/../View/footer/footer.html"); ?>
 
 <script>
+function displaySelectedPicture(input) {
+  var selectedPictureDisplay = document.getElementById('selectedPictureDisplay');
+
+  if (input.files && input.files[0]) {
+  	var reader = new FileReader();
+
+    reader.onload = function (e) {
+		selectedPictureDisplay.src = e.target.result;
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  } else { selectedPictureDisplay.removeAttribute('src') }
+}
+
 function deletePicture(storagePath) {
 	//Call php to delete picture
 	fetch("http://localhost:8000/picture-editing.php", {
@@ -56,27 +69,41 @@ function deletePicture(storagePath) {
 	window.location.reload();//Refresh the page to see new picture in picture list
 }
 
-function takePicture() {
+function takePicture(camera) {
 	const takePictureButton = document.getElementById('takePictureButton');	
 
 	takePictureButton.addEventListener("submit", () => {
   	 	let canvas = document.getElementById('takePictureCanvas');
-      	let canvasContext = canvas.getContext('2d');
-  	 	const stream = document.querySelector('video');
+		let canvasContext = canvas.getContext('2d');
 		let overlayImg = new Image();
 		selectedOverlayImage = document.querySelector('input[name="overlay"]:checked').value;
 		overlayImg.src = `overlayImages/${selectedOverlayImage}.png`
+
+		console.log('before');
+		if (camera) {
+			console.log('NOT IN');
+			const stream = document.querySelector('video');
+			canvasContext.drawImage(stream, 0, 0, canvas.width, canvas.height);
+		} else {
+			console.log('IN');
+			const selectedImage = document.getElementById('selectedPictureDisplay');	
+			canvasContext.drawImage(selectedImage, 0, 0, canvas.width, canvas.height);
+		}
 		
-		canvasContext.drawImage(stream, 0, 0, canvas.width, canvas.height);
 		overlayImg.onload = () => {
+			console.log('1');
 			canvasContext.drawImage(overlayImg, 0, 0, canvas.width, canvas.height);	
+			console.log('2');
 			let imageData = canvas.toDataURL('image/png');
+			console.log('3');
 			//Call php to create picture in database
+			console.log(imageData);
 			fetch("http://localhost:8000/picture-editing.php", {
 				method: "POST",
 				headers: {"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"},
         		body: `imageData=${imageData}`
 			});
+			console.log('4');
 			window.location.reload();//Refresh the page to see new picture in picture list
 		}
 	});
@@ -100,19 +127,28 @@ function streamError(error) {
   if (error.name === 'NotAllowedError') {
     errorMsg('Permissions have not been granted to use your camera' +
       ', you need to allow the page access to your camera in ' +
-      'order to take pictures.', error);
+      'order to take pictures.');
   } else {
   	errorMsg(`getUserMedia error: ${error.name}`, error);
   }
 }
 
 async function init() {
+  var getPictureHTML = document.getElementById('getPicture');
+
   try {
     const stream = await navigator.mediaDevices.getUserMedia({video: true});
+  	getPictureHTML.insertAdjacentHTML('afterbegin', 
+		"<video width='320' height='240' autoplay></video><br>");
 	setupVideoStream(stream);
-	takePicture();
+	takePicture(true);
   } catch (error) {
-    streamError(error);
+	getPictureHTML.insertAdjacentHTML('afterbegin', "<p id='error'></p>" + 
+	"<input type='file' name='selectedPicture' accept='image/*' " + 
+	"onchange='displaySelectedPicture(this);'>" + 
+	"<br><img id='selectedPictureDisplay' width='320' height='240'/>");
+	streamError(error);
+  	takePicture(false);
   }
 }
 
